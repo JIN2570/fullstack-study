38191f0dfa72a873008253f0c9c455721b4c8049
JIN2570 <poll_down011@naver.com>
2023-11-22 20:19:57 +0900

    index

--- test_jin/index.js
+++ test_jin/index.js
@@ -1,7 +1,11 @@
-let a = 20;
-a = 10;
-console.log(a);
+console.log('1. 물가져다 주기');
 
-const b = 20;
-b = 20;
-console.log(b);
+setTimeout(() => {
+  console.log('2. 햄버거 주기');
+}, 3000);
+
+setTimeout(() => {
+  console.log('3. 감자튀김 주기');
+});
+
+console.log('4. 콜라 가져다 주기');
