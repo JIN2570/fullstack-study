a40ed0b84d54fdf29f6914a4cc297ea8f23605f3
JIN2570 <poll_down011@naver.com>
2023-11-29 00:26:11 +0900

    mogodb

--- MogoDB/JinProject/app.js
+++ MogoDB/JinProject/app.js
@@ -3,26 +3,26 @@ const uri =`mongodb://127.0.0.1:27017`;
 const client = new MongoClient(uri);
 
 async function run() {
-  const database = client.db('firstDB');
-  const users = database.collection('users');
+  const database = client.db('firstDB')
+  const users = database.collection('users')
 
-  // const userDate = await users.insertOne({name: 'Jin', age: 28})
-  // console.log('result', userDate);
+  // const userData = await users.insertOne({name: 'JIN', age: 17});
+  // console.log('result', userData);
 
-  // const userList = [{name: '철수', age: 30}, {name: 'jessica', age: 25}]
-  // const userListResult = await users.insertMany(userList)
+  // const userList = [{name: '철수', age: 30},{name: 'jessica', age: 25} ]
+  // const userListResult = await users.insertMany(userList);
   // console.log('result', userListResult);
-
-  // const findUser = await users.findOne({age:{$gt:20}});
+  
+  // const findUser = await users.find({age:{$gt: 20}}).toArray();
   // console.log('result', findUser);
 
-  // const updateUser = await users.updateOne({name: 'Jin'}, {$set:{age: 29}})
+  // const updateUser = await users.updateOne({name: 'JIN'},{$set:{age:18}});
   // console.log('fff', updateUser);
 
-  // const deletUsers = await users.deleteMany({age:{$gt:28}});
-  // console.log('ddd', deletUsers);
+  // const deleteUser = await users.deleteMany({age:{$gt:20}});
+  // console.log('dddd', deleteUser);
 
-  const userData = await users. find({name: 'Jin'}).project({name: 1, _id: 0}).toArray();
+  const userData = await users.find({name: 'JIN'}).project({name: 1,_id:0}).toArray();
   console.log('userdata', userData);
-}
+};
 run();
